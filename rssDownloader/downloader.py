b3775dfe8716a2b809e7c97851ea7244fe36d5c6
bossman7309 <85088259+bossman7309@users.noreply.github.com>
2023-07-10 09:25:25 -0500

    Update downloader.py

--- rssDownloader/downloader.py
+++ rssDownloader/downloader.py
@@ -3,14 +3,31 @@ import youtube_dl
 import schedule
 import time
 import os
+import logging
 
-# Retrieve the RSS feed URL and download directory from the environment variables
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+# Set the RSS feed URL
 feed_url = os.environ.get('RSS_FEED_URL', 'https://example.com/rss')
-download_dir = os.environ.get('DOWNLOAD_DIR', '/portainer/Files/AppData/Config/rssDownloader')
+
+# Set the download directory directly
+download_dir = '/media/bossman7309/ORANGEHHD1/uploads'  # Change this to your desired pat /portainer/Files/AppData/Config/rssDownloader
+
+# Log the directory
+logging.info(f'Set download directory: {download_dir}')
+
+logging.info('everything seems to be working')
 
 def download_videos():
+    logging.info('Starting video download job...')
+
     # Parse the feed
-    feed = feedparser.parse(feed_url)
+    try:
+        feed = feedparser.parse(feed_url)
+    except Exception as e:
+        logging.error(f'Failed to parse feed: {feed_url} - {str(e)}')
+        return
 
     # Setup youtube_dl options
     ydl_opts = {
@@ -24,8 +41,16 @@ def download_videos():
             # If the link is a video
             if "video" in link.type:
                 # Download the video using youtube_dl
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([link.href])
+                try:
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download([link.href])
+                    logging.info(f'Successfully downloaded video: {link.href}')
+                except PermissionError:
+                    logging.error(f'Permission denied when trying to download video: {link.href}')
+                except Exception as e:
+                    logging.error(f'Failed to download video: {link.href} - {str(e)}')
+
+    logging.info('Video download job completed.')
 
 # Schedule the job every 24 hours
 schedule.every(24).hours.do(download_videos)
