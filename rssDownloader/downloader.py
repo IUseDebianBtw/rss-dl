2a2844e41cceab7b049fd197f65cbf4f517838b2
bossman7309 <85088259+bossman7309@users.noreply.github.com>
2023-07-12 10:13:50 -0500

    Update downloader.py

--- rssDownloader/downloader.py
+++ rssDownloader/downloader.py
@@ -27,6 +27,7 @@ logging.info('''
 @******************************@
 @%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@
            RSS-dl
+           
 ''')
 
 # Set the RSS feed URL
@@ -57,21 +58,31 @@ def download_videos():
         'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
     }
 
+    # Get the current time (now) and 24 hours ago (one_day_ago)
+    now = datetime.now()
+    one_day_ago = now - timedelta(days=1)
+
     # For each entry in the feed
     for entry in feed.entries:
-        # For each link in the entry
-        for link in entry.links:
-            # If the link is a video
-            if "video" in link.type:
-                # Download the video using youtube_dl
-                try:
-                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                        ydl.download([link.href])
-                    logging.info(f'Successfully downloaded video: {link.href}')
-                except PermissionError:
-                    logging.error(f'Permission denied when trying to download video: {link.href}')
-                except Exception as e:
-                    logging.error(f'Failed to download video: {link.href} - {str(e)}')
+        # Get the published time of the entry
+        published_time = datetime(*entry.published_parsed[:6])
+
+        # If the entry was published in the last 24 hours
+        if one_day_ago <= published_time <= now:
+            # For each link in the entry
+            for link in entry.links:
+                # If the link is a video
+                if "video" in link.type:
+                    # Download the video using youtube_dl
+                    try:
+                        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                            ydl.download([link.href])
+                        logging.info(f'Successfully downloaded video: {link.href}')
+                    except PermissionError:
+                        logging.error(f'Permission denied when trying to download video: {link.href}')
+                    except Exception as e:
+                        logging.error(f'Failed to download video: {link.href} - {str(e)}')
+
 
 # Schedule the job every 24 hours
 schedule.every(24).hours.do(download_videos)
