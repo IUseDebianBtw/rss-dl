0fcb7ac69ea3b1b38d61f75cb56085ea7334fce3
bossman7309 <85088259+bossman7309@users.noreply.github.com>
2023-07-12 16:21:55 -0500

    Delete directory_setup.py

--- directory_setup.py
+++
@@ -1,9 +0,0 @@
-import os
-import logging
-
-def setup_directory(download_dir):
-    if not os.path.exists(download_dir):
-        os.makedirs(download_dir)
-        logging.info(f'Download directory did not exist. Created directory: {download_dir}')
-    else:
-        logging.info(f'Download directory exists. No need to create one.')
