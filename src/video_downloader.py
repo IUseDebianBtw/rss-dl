072362d59a3fd6ce705c5f63a043028ed3b7835b
bossman7309 <85088259+bossman7309@users.noreply.github.com>
2023-07-14 12:14:05 -0500

    Update video_downloader.py

--- src/video_downloader.py
+++ src/video_downloader.py
@@ -1,36 +1,84 @@
+import os
+import requests
+import logging
+import schedule
+import time
 import feedparser
 import yt_dlp as youtube_dl
-import os
-import logging
-import requests
+from directory_setup import setup_directory
+from video_downloader import download_videos
 
-def download_videos(youtube_url, download_dir):
-    ydl_opts = {
-        'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
-    }
+# Initialize logging configuration
+def initialize_logging():
+    """Initializes a basic logging setup with level set to INFO."""
+    logging.basicConfig(level=logging.INFO)
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    with open('ascii_art.txt', 'r') as f:
+        ascii_art = f.read()
+        logging.info(ascii_art)
 
-    # Download the video using youtube_dl
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([youtube_url])
-        logging.info(f'Successfully downloaded video: {youtube_url}')
-    except PermissionError:
-        logging.error(f'Permission denied when trying to download video: {youtube_url}')
-    except Exception as e:
-        logging.error(f'Failed to download video: {youtube_url} - {str(e)}')
+# Initialize the channel URL
+def initialize_channel_url():
+    """Reads CHANNEL_URL from environment variables and logs it."""
+    channel_url = os.getenv('CHANNEL_URL', 'https://www.youtube.com/@MentalOutlaw')
+    logging.info(f'Channel URL: {channel_url}')
+    return channel_url
 
-def download_videos_from_feed(feed_url, download_dir):
-    # Parse the feed
-    feed = feedparser.parse(feed_url)
-    line_separated_json = '\n'.join(str(feed).split(','))
-    logging.info(f'Feed: {line_separated_json}')
-    
+# Get the channel ID
+def get_browse_id(channel_url):
+    """Retrieves the browse ID for the YouTube channel."""
+    response = requests.get(channel_url)
+    browse_id = response.text.split('"browseId":"')[1].split('"')[0]
+    logging.info(f'Channel ID: {browse_id}')
+    return browse_id
 
-    # For each entry in the feed
-    for entry in feed.entries:
-        # For each link in the entry
-        for link in entry.links:
-            # If the link is a video
-            if 'youtube.com/watch?v=' in link.href:
-                # Download the video
-                download_videos(link.href, download_dir)
+# Set the feed URL
+def set_feed_url(browse_id):
+    """Generates the feed URL using the browse ID."""
+    feed_url = f"https://www.youtube.com/feeds/videos.xml?channel_id={browse_id}"
+    logging.info(f'Feed URL: {feed_url}')
+    return feed_url
+
+# Initialize the download directory
+#def initialize_download_dir():
+#    """Reads DOWNLOAD_DIR from environment variables, creates it if not exists, and logs it."""
+#    download_dir = os.getenv('DOWNLOAD_DIR')
+#    setup_directory(download_dir)
+#    logging.info(f'Set download directory: {download_dir}') #
+#    return download_dir
+
+
+def initialize_download_dir():
+    """Creates the download directory if not exists and logs it."""
+    download_dir = '/home/bossman7309/Videos'  # Set the download directory manually
+    setup_directory(download_dir)
+    logging.info(f'Set download directory: {download_dir}')
+    return download_dir
+
+
+# Download videos
+def initialize_video_downloads(feed_url, download_dir):
+    """Downloads the videos and sets up the schedule for subsequent downloads."""
+    logging.info('Initiating video download')
+    download_videos(feed_url, download_dir)
+    logging.info('Everything seems to be working')
+
+    # Schedule the job every 24 hours
+    schedule.every(24).hours.do(download_videos, feed_url, download_dir)
+
+    # Uncomment below lines for testing purposes
+    # schedule.every(1).minutes.do(download_videos, feed_url, download_dir) 
+    # schedule.every(30).seconds.do(download_videos, feed_url, download_dir)
+
+    # Start the scheduler
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    initialize_logging()
+    channel_url = initialize_channel_url()
+    browse_id = get_browse_id(channel_url)
+    feed_url = set_feed_url(browse_id)
+    download_dir = initialize_download_dir()
+    initialize_video_downloads(feed_url, download_dir)
